4be49651fc57aaae29eddcd32b4663a91a3820b9
Damian Edwards <damian@damianedwards.com>
2011-11-08 22:43:44 -0800

    added some basic CSS and a color flash when a stock updates in the table

--- SignalR.StockTicker/SignalR.StockTicker/SignalR.StockTicker/SignalR.StockTicker.js
+++ SignalR.StockTicker/SignalR.StockTicker/SignalR.StockTicker/SignalR.StockTicker.js
@@ -11,16 +11,25 @@ if (!String.prototype.supplant) {
     };
 }
 
+jQuery.fn.flash = function (color, duration) {
+    var current = this.css('backgroundColor');
+    this.animate({ backgroundColor: 'rgb(' + color + ')' }, duration / 2);
+    this.animate({ backgroundColor: current }, duration / 2);
+}
+
 $(function () {
 
     var ticker = $.connection.stockTicker,
-        $stockTable = $("#stockTable"),
-        $stockTableBody = $stockTable.find("tbody"),
-        rowTemplate = "<tr data-symbol='{Symbol}'><td>{Symbol}</td><td>{Price}</td><td>{DayOpen}</td><td>{DayHigh}</td><td>{DayLow}</td><td>{PercentChange}</td></tr>";
+        $stockTable = $('#stockTable'),
+        $stockTableBody = $stockTable.find('tbody'),
+        rowTemplate = '<tr data-symbol="{Symbol}"><td>{Symbol}</td><td>{Price}</td><td>{DayOpen}</td><td>{DayHigh}</td><td>{DayLow}</td><td>{PercentChange}</td></tr>';
 
     ticker.updateStockPrice = function (stock) {
-        $stockTableBody.find("tr[data-symbol=" + stock.Symbol + "]")
-            .replaceWith(rowTemplate.supplant(stock));
+        var $row = $(rowTemplate.supplant(stock));
+        $stockTableBody.find('tr[data-symbol=' + stock.Symbol + ']')
+            .replaceWith($row);
+        // TODO: Make it flash red/green depending on whether it went down/up
+        $row.flash('255,255,0', 1000);
     };
 
     $.connection.hub.start(function () {
