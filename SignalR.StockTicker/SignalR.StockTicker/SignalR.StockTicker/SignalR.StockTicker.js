9108bafbc19877db5ab08b57b583800154c526f3
Pawel Kadluczka <pawelka@microsoft.com>
2017-05-04 15:09:39 -0700

    Updated to 2.2.2

--- SignalR.StockTicker/SignalR.StockTicker/SignalR.StockTicker/SignalR.StockTicker.js
+++ SignalR.StockTicker/SignalR.StockTicker/SignalR.StockTicker/SignalR.StockTicker.js
@@ -1,5 +1,5 @@
 ﻿/// <reference path="../Scripts/jquery-1.10.2.js" />
-/// <reference path="../Scripts/jquery.signalR-2.1.1.js" />
+/// <reference path="../Scripts/jquery.signalR-2.2.2.js" />
 
 /*!
     ASP.NET SignalR Stock Ticker Sample
